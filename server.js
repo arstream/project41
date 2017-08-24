eaccf932d9774a8417519e8704e3bc9fedad9de2
vagrant <vagrant@vagrant.vm>
2017-08-24 11:52:48 +0000

    second commit

---
+++ server.js
@@ -0,0 +1,13 @@
+// server.js
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+var port = process.env.PORT || 3000;
+
+server.use(middlewares)
+server.use(router)
+server.listen(port, () => {
+  console.log('json-server is running on port ' + port + '!')
+})
