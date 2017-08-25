945fe4a5d259116c84e8c0d1807530d1835cadfa
vagrant <vagrant@vagrant.vm>
2017-08-25 10:38:16 +0000

    adjust package.json

--- server.js
+++ server.js
@@ -6,6 +6,20 @@ const middlewares = jsonServer.defaults()
 
 var port = process.env.PORT || 3000;
 
+var basicAuth = require('express-basic-auth')
+ 
+server.use(basicAuth({
+    users: { 'admin': 'secret' },
+    challenge: true,
+    unauthorizedResponse: getUnauthorizedResponse,
+}))
+
+function getUnauthorizedResponse(req) {
+    return req.auth ?
+        {'Unauthorized':('Credentials ' + req.auth.user + ':' + req.auth.password + ' rejected')} : 
+        {'Unauthorized':'No credentials provided'}
+}
+
 server.use(middlewares)
 server.use(router)
 server.listen(port, () => {
